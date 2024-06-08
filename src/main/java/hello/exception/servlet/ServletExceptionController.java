e0b54c45523eff20f8d45e2752fd77174314589e
cheoljundev <cheoljundev@gmail.com>
2024-06-09 02:14:44 +0900

    서블릿 예외 처리 - 오류 화면 제공

--- src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -15,12 +15,12 @@ public class ServletExceptionController {
         throw new RuntimeException("예외 발생!");
     }
 
-    @GetMapping("error-404")
+    @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
     }
 
-    @GetMapping("error-500")
+    @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
     }
